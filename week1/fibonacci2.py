aaceb2a177a277a076f9571482e03f2f426e0840
cwang999 <89167181+cwang999@users.noreply.github.com>
2022-03-29 15:12:29 -0700

    Create fibonacci2.py

---
+++ week1/fibonacci2.py
@@ -0,0 +1,32 @@
+def fib_seq(n):
+    if n == 0:
+        print(0)
+        return
+        
+    if n < 0:
+        raise Exception("Can't use negative n")
+    
+    def helper(idx, curr, prev):
+        print(curr)
+        if idx == n:
+            return
+        helper(idx + 1, curr + prev, curr)
+    
+    print(0)
+    helper(1, 1, 0)
+  
+        
+def print_fibo(n):
+    try:
+        fib_seq(n)
+    except:
+        print("Invalid n")
+
+
+def tester():
+  while True:
+    f = input('Which Fibonacci number to go up to?: ')
+    print_fibo(int(f)-1)
+
+
+tester()
