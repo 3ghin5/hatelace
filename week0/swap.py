6f84baf54a023b23c25bb5cbb8aba494179a0754
cwang999 <89167181+cwang999@users.noreply.github.com>
2022-03-29 15:26:39 -0700

    Update swap.py

--- week0/swap.py
+++ week0/swap.py
@@ -13,3 +13,11 @@ def driver():
   except:
     print("Invalid input; please input a number")
   swap(a,b)
+  
+  
+# reverses order of numbers
+num1 = int(input("Input a number"))
+num2 = int(input("Input another number"))
+def swapNum(x, y):
+  return num2, num1
+print(swapNum(num1,num2))
